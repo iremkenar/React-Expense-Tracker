cb288521d1c9b285fbdea6de9707fa1afc35b9c6
iremkenar <irem.kenar@outlook.com>
2021-04-24 17:56:36 +0300

    Added expense item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
