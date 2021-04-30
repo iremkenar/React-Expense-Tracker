31dbf764ee838731d6322ebbf5920db0554fd289
iremkenar <irem.kenar@outlook.com>
2021-04-30 15:26:34 +0300

    Added arrow functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses';
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -28,6 +28,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
