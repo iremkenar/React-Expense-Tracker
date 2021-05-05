8629672cc8d6d3f69ed5961182708fa05c17c1d8
iremkenar <irem.kenar@outlook.com>
2021-05-05 18:27:48 +0300

    Added new expense editing functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -83,6 +83,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [displayedContent, setDisplayContent] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setDisplayContent(false);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-  );
+  const cancelNewExpenseHandler = (event) => {
+    event.preventDefault();
+    setDisplayContent(false);
+  };
+
+  const clickHandler = () => {
+    setDisplayContent(true);
+  };
+
+  let initialDisplay = <button onClick={clickHandler}>Add New Expense</button>;
+  if (displayedContent) {
+    initialDisplay = (
+      <ExpenseForm
+        onCancel={cancelNewExpenseHandler}
+        onSaveExpenseData={saveExpenseDataHandler}
+      />
+    );
+  }
+
+  return <div className="new-expense">{initialDisplay}</div>;
 };
 
 export default NewExpense;
